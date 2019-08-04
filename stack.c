1c8fd23547344533d7f4a7861dc74b0a3574191c
ovedharuvi <oved1991@gmail.com>
2019-08-04 18:48:37 +0300

    first commit

--- stack.c
+++ stack.c
@@ -4,23 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct {
-    int is_fixed; /*0 if fixed 1 if not fixed */
-    int value; /*0 if empty*/
-    int solution_value; /*0 if empty*/
-    int *potentialValues;
-    int numOfPotentialValues;
-} cell;
 
-struct stackNode {
-    int row;
-    int column;
-    int direction;
-    cell **board;
-    struct stackNode* next;
-} ;
-
-typedef struct stackNode Iteration;
 Iteration* top;
 
 Iteration *createSolveItaration(int row , int column , int direction , cell **board){
--- stack.h
+++ stack.h
@@ -2,4 +2,25 @@
 #ifndef SUDOKOPROJECT_STACK_H
 #define SUDOKOPROJECT_STACK_H
 
+typedef struct {
+    int is_fixed; /*0 if fixed 1 if not fixed */
+    int value; /*0 if empty*/
+    int solution_value; /*0 if empty*/
+    int *potentialValues;
+    int numOfPotentialValues;
+} cell;
+
+struct stackNode {
+    int row;
+    int column;
+    int direction;
+    cell **board;
+    struct stackNode* next;
+} ;
+
+typedef struct stackNode Iteration;
+
+void Push(int row , int column , int direction , cell** board);
+Iteration* Top();
+void Pop();
 #endif //SUDOKOPROJECT_STACK_H
