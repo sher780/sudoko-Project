fce91cc03a224753b28afb0ac67f0f8d7c130f6e
sher780 <41858809+sher780@users.noreply.github.com>
2019-08-16 10:54:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+#include "parser.h"
+
+int mod = 1; /*set to init*/
 int main() {
-    printf("Hello, World!\n");
+    while (mod!=-1) {
+        int order();
+    }
+    exit();/*function that releases everything*/
     return 0;
-}
